a708065899217b233c006decd8687025f09ec6d4
theoweuh <39169618+theoweuh@users.noreply.github.com>
2022-09-06 11:57:59 +0200

    Classe Souri AGE/ESPERANCE-VIE

--- Souris.java
+++ Souris.java
@@ -1,3 +1,16 @@
 public class Souris {
 
+    int modelage;
+    int modelesperanceVie;
+
+    public Souris(int age, int esperanceVie) {
+        modelage = age;
+        modelesperanceVie = esperanceVie;
+    }
+
+    public static void main(int[] args) {
+        Souris souris = new Souris(0, 36);
+        System.out.println(souris.modelage + " " + souris.modelesperanceVie);
+    }
+
 }
